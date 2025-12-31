4de00a199e44d4647337c982d3d1125cf08dde78
arun-s3 <arunsudhakaran01@gmail.com>
2026-01-01 03:08:01 +0530

    Resolved the preview image issues in user profile edit, admin self profile and enabled preview in admin's user profile edit pages and sign up page

--- public/js/login2.js
+++ public/js/login2.js
@@ -1,25 +1,3 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    const inputs = document.querySelectorAll(".inp input");
-
-    inputs.forEach(input => {
-    input.addEventListener("input", function() {
-      const label = this.nextElementSibling;
-      label.classList.toggle("up", this.value.trim() !== "");
-    });
-   });
-});
-
-function focusinp(inp) {
-if (inp == 'usr') {
-document.getElementById("username").focus();
-} else if (inp == 'pass') {
-document.getElementById("password").focus();
-} else {
-document.getElementById("username").focus();
-}
-}
-
 const email = document.getElementById("email")
 const error1 = document.getElementById("error1")
 

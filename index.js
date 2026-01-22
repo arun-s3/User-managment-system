7adfe607539e99ea90ef573df62c1d7f53bab82d
arun-s3 <arunsudhakaran01@gmail.com>
2026-01-23 03:09:03 +0530

    Fixed Content-Security-Policy to allow images and static assets

--- index.js
+++ index.js
@@ -15,10 +15,28 @@ mongoose.connection.on("error", ()=>{console.log("/n Error in database connectio
 
 
 app.use(nocache())
+
+app.use((req, res, next) => {
+    res.setHeader(
+        "Content-Security-Policy",
+            `
+                default-src 'self';
+                img-src 'self' data: https:;
+                style-src 'self' 'unsafe-inline' https:;
+                script-src 'self' 'unsafe-inline' https:;
+                font-src 'self' https:;
+            `
+            .replace(/\s{2,}/g, " ")
+            .trim(),
+    )
+    next()
+})
+
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
 // app.use(express.static("public"))
 app.use('/', require("./routes/userRoute"));
 app.use('/admin', require("./routes/adminRoute"));
+
 
 app.listen(process.env.PORT||5000, ()=>console.log("The server is running..."));
